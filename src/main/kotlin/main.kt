6fdfb061904b4d13bed8212bf8506576ee165660
Dmitry Romanov <me@wutiarn.ru>
2016-03-14 20:25:24 +0300

    Uploadimg all files from folder

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,19 +1,19 @@
 import com.mashape.unirest.http.Unirest
 import java.io.File
-import java.io.FileNotFoundException
 
 /**
  * Created by wutiarn on 14.03.16.
  */
 fun main(args: Array<String>) {
-    val file = File(args[0])
+    val file = File("/Users/wutiarn/edustor")
+    val files = file.listFiles { file, name -> name.endsWith(".pdf") }
 
-    if (file.exists() == false) throw FileNotFoundException("Specified file isn't found")
-
-    val result = Unirest.post("http://wutiarn.ru/api/documents/upload")
-            .header("token", "a7933bb1-7d01-4db0-91b6-419412dd85c9")
-            .field("file", file, "application/pdf")
-            .asString()
-
-    println("Status: ${result.status}. Message: ${result.body}")
+    files.forEach {
+        val response = Unirest.post("http://wutiarn.ru/api/documents/upload")
+                .header("token", "a7933bb1-7d01-4db0-91b6-419412dd85c9")
+                .field("file", it, "application/pdf")
+                .asString()
+        println("Uploaded ${it.name}. Status: ${response.status}. Message: ${response.status}")
+        if (response.status == 200) it.delete()
+    }
 }
