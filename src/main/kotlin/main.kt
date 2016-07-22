c2c9de52918093c01be2047bbd7ca9897e630709
Dmitry Romanov <me@wutiarn.ru>
2016-07-22 16:23:29 +0300

    edustor url fix

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,12 @@
 import com.mashape.unirest.http.Unirest
 import java.io.File
 
-/**
- * Created by wutiarn on 14.03.16.
- */
 fun main(args: Array<String>) {
     val file = File("/Users/wutiarn/edustor")
     val files = file.listFiles { file, name -> name.endsWith(".pdf") }
 
     files.forEach {
-        val response = Unirest.post("http://wutiarn.ru/api/documents/upload")
+        val response = Unirest.post("https://edustor.ru/api/documents/upload")
                 .header("token", "a7933bb1-7d01-4db0-91b6-419412dd85c9")
                 .field("file", it, "application/pdf")
                 .asString()
